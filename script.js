4a187d260754c36d41d52f114bd2677bd9610cbc
LochaydeOliveira <lochaydeguerreiro@hotmail.com>
2022-08-17 09:30:56 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+function textReturn1 (){
+    return "Função 01 - Olá"
+    
+
+}
+console.log(textReturn1())
+
+function textReturn (text){
+    return text
+    
+
+}
+
+console.log(textReturn("Função 02 - Olá, estou sendo retorndo pela função"))
+
+function textReturn (text){
+    console.log(text)
+    
+}
+textReturn("Função 03 - Olá")
